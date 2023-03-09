e9f7662d5d216338314faecc4a1c2e7c9a9af55d
Daniel <subiabrewd@gmail.com>
2023-03-09 19:47:45 +0100

    Read user preferences from localStorage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
-<script>
-  import "../app.scss";
+<script lang="ts">
+    import "../app.scss";
+    import { auth } from "$lib/stores";
+    import { onMount } from "svelte";
+    import { getEffects } from "$lib/human/effects";
+
+    onMount(() => {
+        const prefers = getEffects($auth?.user);
+
+        if (prefers.fx) {
+            document.body.classList.add("has-fxs");
+        }
+    });
 </script>
 
 <slot />
