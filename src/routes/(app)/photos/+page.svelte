aed213cc4475f92eca051e8a284127ac7fd19133
Daniel <subiabrewd@gmail.com>
2023-03-11 04:41:59 +0100

    Add dummy photos page

---
+++ src/routes/(app)/photos/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import TraySlide from "$lib/layout/TraySlide.svelte";
+</script>
+<TraySlide id="filters">
+    <div class="padded">
+        <h2>Cuándo</h2>
+        <input type="range" />
+    </div>
+    <div class="padded">
+        <h2>Dónde</h2>
+    </div>
+    <div class="padded">
+        <h2>Quién</h2>
+    </div>
+    <div class="padded">
+        <h2>Cómo</h2>
+        <form>
+            <input type="text" placeholder="..." />
+        </form>
+    </div>
+</TraySlide>
+<TraySlide id="result">
+    <div class="padded grid">
+        <figure>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/C77190s1_Ant.Map_Ross_Island.jpg/320px-C77190s1_Ant.Map_Ross_Island.jpg" />
+        </figure>
+        <figure>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/C77190s1_Ant.Map_Ross_Island.jpg/320px-C77190s1_Ant.Map_Ross_Island.jpg" />
+        </figure>
+        <figure>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/C77190s1_Ant.Map_Ross_Island.jpg/320px-C77190s1_Ant.Map_Ross_Island.jpg" />
+        </figure>
+        <figure>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/C77190s1_Ant.Map_Ross_Island.jpg/320px-C77190s1_Ant.Map_Ross_Island.jpg" />
+        </figure>
+    </div>
+</TraySlide>
