ac0fa5d0c95dfa1147f9aa081d792e1daf2e784e
Daniel <subiabrewd@gmail.com>
2023-03-12 03:27:00 +0100

    Show QR of key

--- src/routes/(app)/user/KeyListItem.svelte
+++ src/routes/(app)/user/KeyListItem.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { api } from "$lib/api";
     import { auth } from "$lib/stores";
+    import { qr } from "$lib/human/qr";
     import { time } from "$lib/human/time";
     import { shorten } from "$lib/human/shorten";
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
     import type { UserKey } from "$lib/api/types/User";
 
     export let key: UserKey;
@@ -11,6 +12,7 @@
     const dispatch = createEventDispatcher();
 
     let keyCopy = false;
+    let canvas: HTMLCanvasElement;
 
     $: url = `${window.location.origin}/login?key=${key.value}`;
     $: value = shorten.hyphened(key.value, 15);
@@ -31,6 +33,10 @@
             .delete($auth.user.id, key.id)
             .then(() => dispatch("deletion", { key }));
     }
+
+    onMount(() => {
+        qr.toCanvas(canvas, url);
+    });
 </script>
 
 <form on:submit={handleSubmit}>
@@ -38,6 +44,7 @@
     <p>{value}</p>
     <p>Creada <span class="font-weight-medium">{dateCreated}</span></p>
     <p>Caduca <span class="font-weight-medium">{dateExpires}</span></p>
+    <p><canvas bind:this={canvas} /></p>
     <p>
         {#if keyCopy}
             <span class="button color-success">Enlace copiado</span>
