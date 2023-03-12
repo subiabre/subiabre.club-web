3b81ceb7e47244e4ed0375249da597cc1e86eab9
Daniel <subiabrewd@gmail.com>
2023-03-12 09:03:55 +0100

    Start location filter

--- src/routes/(app)/photos/FilterLocation.svelte
+++ src/routes/(app)/photos/FilterLocation.svelte
@@ -27,43 +27,62 @@
     );
 </script>
 
-<div class="grid">
-    <ul>
-        {#await countries then countries}
-            {#each countries as country}
-                <li>{country.country}</li>
-            {/each}
-        {/await}
-    </ul>
-    <ul>
-        {#await regions then regions}
-            {#each regions as region}
-                <li>{region.region}</li>
-            {/each}
-        {/await}
-    </ul>
-    <ul>
-        {#await subregions then subregions}
-            {#each subregions as subregion}
-                <li>{subregion.subregion}</li>
-            {/each}
-        {/await}
-    </ul>
-    <ul>
-        {#await localities then localities}
-            {#each localities as locality}
-                <li>{locality.locality}</li>
-            {/each}
-        {/await}
-    </ul>
+<div class="grid-180">
+    <div>
+        <h6 class="label">País</h6>
+        <ul>
+            {#await countries then countries}
+                {#each countries as country}
+                    <li>{country.country}</li>
+                {/each}
+            {/await}
+        </ul>
+    </div>
+    <div>
+        <h6 class="label">Región</h6>
+        <ul>
+            {#await regions then regions}
+                {#each regions as region}
+                    <li>{region.region}</li>
+                {/each}
+            {/await}
+        </ul>
+    </div>
+    <div>
+        <h6 class="label">Subregión</h6>
+        <ul>
+            {#await subregions then subregions}
+                {#each subregions as subregion}
+                    <li>{subregion.subregion}</li>
+                {/each}
+            {/await}
+        </ul>
+    </div>
+    <div>
+        <h6 class="label">Localidad</h6>
+        <ul>
+            {#await localities then localities}
+                {#each localities as locality}
+                    <li>{locality.locality}</li>
+                {/each}
+            {/await}
+        </ul>
+    </div>
 </div>
 
 <style lang="scss">
     ul {
-        max-height: 200px;
+        max-height: 90px;
+        overflow-y: scroll;
 
         li {
-            padding: 0.2em 0;
+            padding: 0.2em;
+
+            color: darken($color: $color-body, $amount: 50);
+
+            &:hover {
+                color: $color-body;
+            }
         }
     }
 </style>
