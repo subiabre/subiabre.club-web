bccbe8fd44945b7b90832d13cc2c7c9fb5f25e3f
Daniel <subiabrewd@gmail.com>
2023-04-01 13:18:52 +0200

    Move photo collection logic to photo listing component

--- src/routes/(app)/photos/+layout.svelte
+++ src/routes/(app)/photos/+layout.svelte
@@ -1,24 +1,10 @@
 <script lang="ts">
-    import { api } from "$lib/api";
-    import { filters } from "$lib/stores";
-    import { goto } from "$app/navigation";
     import TraySlide from "$lib/layout/TraySlide.svelte";
     import FilterDate from "./FilterDate.svelte";
     import PhotoList from "./PhotoList.svelte";
     import FilterPlace from "./FilterPlace.svelte";
     import FilterPeople from "./FilterPeople.svelte";
     import FilterDescription from "./FilterDescription.svelte";
-
-    $: photos = api.photos.getCollection(
-        new URLSearchParams([
-            ...$filters.filter((param) => param.length === 2),
-            ["date[order]", "asc"],
-        ])
-    );
-
-    function handleSelection(event: any) {
-        goto(`/photos/${event.detail.photo.id}#item`, { keepFocus: true });
-    }
 </script>
 
 <TraySlide id="filters">
@@ -46,8 +32,6 @@
     <div class="padded">
         <h1>Colección</h1>
     </div>
-    {#await photos then photos}
-        <PhotoList {photos} on:selection={handleSelection} />
-    {/await}
+    <PhotoList />
 </TraySlide>
 <slot />
--- src/routes/(app)/photos/PhotoList.svelte
+++ src/routes/(app)/photos/PhotoList.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
-    import type { Photo } from "$lib/api/types/Photo";
+    import { goto } from "$app/navigation";
+    import { api } from "$lib/api";
+    import { filters } from "$lib/stores";
     import PhotoListItem from "./PhotoListItem.svelte";
 
-    export let photos: Photo[];
+    $: photos = api.photos.getCollection(
+        new URLSearchParams([
+            ...$filters.filter((param) => param.length === 2),
+            ["date[order]", "asc"],
+        ])
+    );
+
+    function handleSelection(event: any) {
+        goto(`/photos/${event.detail.photo.id}#item`, { keepFocus: true });
+    }
 </script>
 
 <section class="padded grid-250">
-    {#each photos as photo (photo.id)}
-        <PhotoListItem {photo} on:selection />
-    {/each}
+    {#await photos then photos}
+        {#each photos as photo (photo.id)}
+            <PhotoListItem {photo} on:selection={handleSelection} />
+        {/each}
+    {/await}
 </section>
 
 <style lang="scss">
